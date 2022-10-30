cef1c882f4fb0ea7b096ef136046be5aef38b786
yasserxkamal <115097103+yasserxkamal@users.noreply.github.com>
2022-10-30 23:12:57 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,4 +1,22 @@
 
 
-print("Hello world")
-print("welcome")
+import Weapon from tp1
+
+from unittest import TestCase
+
+
+class TestclassTP1(TestCase):
+  def testLance_missiles_antisurface(self):
+    w1=Lance_missiles_antisurface().fire_at(5,7,0)
+    self.assertNotEqual("NoAmmunitionError", w1)
+    self.assertNotEqual("OutOfRangeError", w1)
+  def testLance_torpilles(self):
+    w2=Lance_torpilles().fire_at(10,-4,5)
+    self.assertNotEqual("NoAmmunitionError", w2)
+    self.assertNotEqual("OutOfRangeError", w2)
+  def testLance_missiles_antiair(self):
+    w3=Lance_missiles_antiair.fire_at(100,45,0)
+    self.assertNoteEqual("NoAmmunitionError", w3)
+    self.assertNotEqual("OutOfRangeError", w3)
+
+   
