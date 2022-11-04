0bbff8aaa699fdacdf44679900426f0b96c844f7
yasserxkamal <115097103+yasserxkamal@users.noreply.github.com>
2022-11-04 22:49:53 +0100

    Update main.py

--- TP1_folder./main.py
+++ TP1_folder./main.py
@@ -1,1 +1,6 @@
-h
+lance_mi_anti_sur=Lance_missiles_antisurface()
+lance_torp=Lance_torpilles()
+lance_mi_anti_air=Lance_missiles_antiair()
+print(lance_mi_anti_air.fire_at(5,7,5))
+print(lance_torp.fire_at(10,-4,5))
+print(lance_mi_anti_sur.fire_at(100,45,0))
