5914abf9021698bae28f0a0e31be9e9f55723d54
yasserxkamal <115097103+yasserxkamal@users.noreply.github.com>
2022-11-04 22:12:57 +0100

    Update TP2_folde_Création_de la_classe_mère_pour_les_vaisseaux.py.py

--- TP2_folde./TP2_folde_Création_de la_classe_mère_pour_les_vaisseaux.py.py
+++ TP2_folde./TP2_folde_Création_de la_classe_mère_pour_les_vaisseaux.py.py
@@ -1,1 +1,25 @@
-f
+from tp1 import *
+
+
+class Vessel:
+    def __init__(self,coordinates,max_hits,weapon):
+        self.coord=coordinates
+        self.max_hits=max_hits
+        self.weapon=weapon
+
+    def go_to(self,x,y,z):
+        if self.max_hits==0:
+            return('DestroyedError')
+        else:
+            
+            self.coord=(x,y,z)
+            return "I go to ("+str(x)+","+str(y)+","+str(z)+")"
+    
+    def get_coordinates(self):
+        print(self.coord)
+
+    def fire_at(self,x,y,z):
+        if self.max_hits==0:
+            return 'DestroyedError'
+        else:
+            return self.weapon.fire_at(x,y,z)
