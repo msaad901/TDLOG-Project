bdc361a42d54501bcbdb19c26c402aad80b60343
yasserxkamal <115097103+yasserxkamal@users.noreply.github.com>
2022-11-05 12:04:40 +0100

    Update main.py

--- TP2_folde./main.py
+++ TP2_folde./main.py
@@ -22,12 +22,12 @@ Fregate=Fregate((2,2,0),lance_missiles_anti_surface_1,5)
 Destroyer=Destroyer((3,3,0),lance_torpilles_2,4)
 Aircraft=Aircraft((1,1,1),lance_missiles_anti_surface_2,1)
 
-Cruiser.go_to(1,2,10)
-Cruiser.go_to(1,2,-10)
-Cruiser.get_coordinates()
-Cruiser.go_to(5,7,0)
-Cruiser.get_coordinates()
+print(Cruiser.go_to(1,2,10))
+print(Cruiser.go_to(1,2,-10))
+print(Cruiser.get_coordinates())
+print(Cruiser.go_to(5,7,0))
+print(Cruiser.get_coordinates())
 print(Cruiser.weapon.amo)# affiche le nombre d'amo de l'arme
-Cruiser.fire_at(10,10,10)# car c anti_air donc ca marche normal car z>0
-Cruiser.fire_at(10,10,-10) #ici ca affiche OutOfRangeError
+print(Cruiser.fire_at(10,10,10))# car c anti_air donc ca marche normal car z>0
+print(Cruiser.fire_at(10,10,-10)) #ici ca affiche OutOfRangeError
 print(Cruiser.weapon.amo)#le nombre d'amo a diminue de 1 apres OutOfRangeError
